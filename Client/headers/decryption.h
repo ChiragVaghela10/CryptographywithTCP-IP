490d92acefb07ff363453481811a6df4f3f5c1de
Chirag <chiragvaghela10@ymail.com>
2015-02-08 00:21:26 +0530

    Create decryption.h

---
+++ Client/headers/decryption.h
@@ -0,0 +1,6 @@
+#ifndef KEY_GENERATE_H_INCLUDED
+#define KEY_GENERATE_H_INCLUDED
+
+void decryption(long int PRIVATE_EXPO,long int MOD,char *encrypted_values,int n,char *filename);
+
+#endif // DECRYPTION_H_INCLUDED
