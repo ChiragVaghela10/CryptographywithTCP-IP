bdfb6576e7f1b2532a45eb6f7df240d300dda054
Chirag <chiragvaghela10@ymail.com>
2015-02-08 00:22:56 +0530

    Create my_modular_arith.h

---
+++ Client/headers/my_modular_arith.h
@@ -0,0 +1,7 @@
+#ifndef MUL_INV_H_INCLUDED
+#define MUL_INV_H_INCLUDED
+
+long int mul_inv(long int ,long int );
+long long int modular_expo(long int b,long int e,long int m);
+
+#endif // MY_MODULAR_ARITH_H_INCLUDED
