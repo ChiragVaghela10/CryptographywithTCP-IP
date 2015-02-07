e0879f6beba4acddde7c5e457dd725611e1748df
Chirag <chiragvaghela10@ymail.com>
2015-02-08 00:32:18 +0530

    Create encryption.h

---
+++ Server/headers/encryption.h
@@ -0,0 +1,7 @@
+#ifndef KEY_GENERATE_H_INCLUDED
+#define KEY_GENERATE_H_INCLUDED
+
+char * encrypt(long int ,long int ,char *);
+long int modular_expo(long int b,long int e,long int m);
+
+#endif // ENCRYPTION_H_INCLUDED
