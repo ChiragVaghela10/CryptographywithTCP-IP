b7545657a3cf8f49592133b0314a2c51f18e59f2
Chirag <chiragvaghela10@ymail.com>
2015-02-08 00:22:07 +0530

    Create key_generate.h

---
+++ Client/headers/key_generate.h
@@ -0,0 +1,7 @@
+#ifndef KEY_GENERATE_H_INCLUDED
+#define KEY_GENERATE_H_INCLUDED
+
+void key_generate(long int *,long int *,long int *);
+long int generate_primes(long int a[]);
+
+#endif // KEY_GENERATE_H_INCLUDED
